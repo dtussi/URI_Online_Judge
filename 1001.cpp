49508c1958672a21cb81ea6d99922fa324f1b1ac
dtussi <dtussi@yahoo.com>
2017-02-26 12:27:49 -0300

    Add files via upload
    Problem 1001

---
+++ 1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+	// Declara as var A B X
+	int A, B, X;
+	// Leiura das var A e B
+	cin >> A;
+	cin >> B;
+	// Soma e armazenada na var X
+	X = A + B;
+	// Mostra o resultado em X
+	cout << "X = " << X << "\n";
+    return 0;
+}
